bb814dbd494e74d5ee85a59ffb214860e2195d4d
Thomas Perl <m@thp.io>
2012-10-04 12:31:51 +0200

    Windows snapshot: Static linking, build fixes

--- examples/c/tuio_server.cpp
+++ examples/c/tuio_server.cpp
@@ -36,8 +36,8 @@
 #include <string.h>
 
 #ifdef _WIN32
-#  include <Winsock2.h>
-#  include <Ws2tcpip.h>
+#  include <winsock2.h>
+#  include <ws2tcpip.h>
 #else
 #  include <arpa/inet.h>
 #  include <netinet/in.h>
