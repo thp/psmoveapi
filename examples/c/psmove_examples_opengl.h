8aae2fde0ba7e6504245e8a96410fea3637fb402
Thomas Perl <m@thp.io>
2013-06-29 16:44:56 +0200

    Fix OpenGL examples on Mac OS X (fixes #66)

---
+++ examples/c/psmove_examples_opengl.h
@@ -0,0 +1,12 @@
+#ifndef PSMOVE_EXAMPLES_OPENGL_H
+#define PSMOVE_EXAMPLES_OPENGL_H
+
+#ifdef __APPLE__
+  #include <OpenGL/gl.h>
+  #include <GLUT/glut.h>
+#else
+  #include <GL/gl.h>
+  #include <GL/glut.h>
+#endif 
+
+#endif /* PSMOVE_EXAMPLES_OPENGL_H */
