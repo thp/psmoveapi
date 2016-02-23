0c79f591c1b96c660f8a5c1ef544b3a2ba36733d
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2016-02-23 22:10:10 +0100

    - Added submodule init to travis config - Added language setting to travis config - Added apt packages to travis config - No longer installing packages through the build script when running through TravisCI

--- build_scripts/ubuntu/build.sh
+++ build_scripts/ubuntu/build.sh
@@ -12,15 +12,19 @@ fi
 
 cd $PSMOVE_API_ROOT_DIR
 
-# Install required packages
-echo "Installing prerequisites..."
-sudo apt-get update -qq
-sudo apt-get install -q  build-essential cmake 			\
-		     	 libudev-dev libbluetooth-dev 		\
-		     	 libv4l-dev libopencv-dev 		\
-		     	 openjdk-7-jdk ant liblwjgl-java 	\
-		     	 python-dev mono-mcs 			\
-		     	 swig3.0 libsdl2-dev freeglut3-dev
+# Only install packages here if not running through TravisCI; 
+# it has its own system for installing packages (and not all of them are available)
+if [ -z $TRAVIS ]
+then
+	echo "Installing prerequisites..."
+	sudo apt-get update -qq
+	sudo apt-get install -q  build-essential cmake 			\
+			     	 libudev-dev libbluetooth-dev 		\
+			     	 libv4l-dev libopencv-dev 		\
+			     	 openjdk-7-jdk ant liblwjgl-java 	\
+			     	 python-dev mono-mcs 			\
+			     	 swig3.0 libsdl2-dev freeglut3-dev
+fi
 
 # Configure
 echo "Configuring build..."
