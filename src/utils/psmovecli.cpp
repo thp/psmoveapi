07f5ed7e609497e5bccb5f5c404355206033e434
Thomas Perl <m@thp.io>
2016-11-30 12:20:19 +0100

    Merge devtools into psmovecli

--- src/utils/psmovecli.cpp
+++ src/utils/psmovecli.cpp
@@ -31,13 +31,25 @@ struct SubCommand {
 #include "magnetometer_calibration.c"
 #undef main
 
+#define main dfu_mode_main
+#include "psmove_dfu_mode.c"
+#undef main
+
+#define main get_firmware_info_main
+#include "psmove_get_firmware_info.c"
+#undef main
+
+#define main auth_response_main
+#include "psmove_auth_response.c"
+#undef main
+
 static int
 usage(const char *progname, std::vector<SubCommand> &subcommands)
 {
     printf("Usage: %s <cmd>\n\nWhere <cmd> is one of:\n", progname);
 
     for (auto &cmd: subcommands) {
-        printf("    %-10s ..... %s\n", cmd.cmd, cmd.help);
+        printf("    %-15s ... %s\n", cmd.cmd, cmd.help);
     }
     printf("\n");
 
@@ -53,6 +65,9 @@ main(int argc, char *argv[])
     subcommands.emplace_back("daemon", "Server locally connected controllers via UDP", moved_main);
     subcommands.emplace_back("register", "Register already paired controllers", psmoveregister_main);
     subcommands.emplace_back("calibrate", "Calibrate magnetometers of contollers", magnetometer_calibration_main);
+    subcommands.emplace_back("dfu-mode", "Switch to DFU mode (potentially dangerous)", dfu_mode_main);
+    subcommands.emplace_back("firmware-info", "Show information about the controller firmware", get_firmware_info_main);
+    subcommands.emplace_back("auth-response", "Challenge-response authentication dev tool", auth_response_main);
 
     if (argc == 1 || strcmp(argv[1], "help") == 0) {
         return usage(argv[0], subcommands);
