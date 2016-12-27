f74fc76163712443bc82e86cf398c6e856f60db8
Thomas Perl <m@thp.io>
2016-12-27 19:12:32 +0100

    Also install python-sphinx

--- scripts/pocketchip/install_dependencies.sh
+++ scripts/pocketchip/install_dependencies.sh
@@ -9,4 +9,5 @@ sudo apt-get update -qq
 sudo apt-get install -q -y build-essential cmake             \
                            libudev-dev libbluetooth-dev      \
                            libv4l-dev libopencv-dev          \
-                           python-dev swig3.0 freeglut3-dev
+                           python-dev swig3.0 freeglut3-dev  \
+                           python-sphinx
