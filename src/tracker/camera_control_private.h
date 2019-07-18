257a4ef118d739950750d77cd9592a443d6c5b99
Aaron Angert <aangertdev@gmail.com>
2019-07-18 18:00:33 -0500

    updated to opencv 3

--- src/tracker/camera_control_private.h
+++ src/tracker/camera_control_private.h
@@ -33,6 +33,7 @@
 #include "opencv2/core/core_c.h"
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/videoio/videoio_c.h"
 
 #if defined(WIN32)
 #    include <windows.h>
